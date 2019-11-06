86d07621c92c4d40679ab26197b8cf14a934aa1c
Prawee Wongsa <prawee@hotmail.com>
2019-11-07 02:21:53 +0700

    update styling of search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,6 +6,7 @@ const SearchBar = () => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather 
+        style={styles.iconStyle}
         name="search"
         size={30}
       />
@@ -19,6 +20,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   backgroundStyle: {
+    marginTop: 15,
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
@@ -26,9 +28,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   inputStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
-    flex: 1
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
   }
 })
 
