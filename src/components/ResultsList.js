39cd80b73b17cf72fd119fdc8c4da538be9bc07f
Prawee Wongsa <prawee@hotmail.com>
2019-11-09 00:43:08 +0700

    create ResultsList comp

---
+++ src/components/ResultsList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+
+const ResultsList = () => {
+  return (
+    <View>
+      <Text>ResultList</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ResultsList
