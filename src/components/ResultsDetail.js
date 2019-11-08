9aba5c1398dae7ac7974dc040dcb7291948946f0
Prawee Wongsa <prawee@hotmail.com>
2019-11-09 01:02:43 +0700

    create ResultsDetail for show single result

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Image, Text, StyleSheet } from 'react-native'
+
+const ResultsDetail = () => {
+  return (
+    <View>
+      <Text>Results detail</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ResultsDetail
