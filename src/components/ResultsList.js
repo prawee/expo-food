0196318d4d502dc2c0d457a992f85fb29ec610c8
Prawee Wongsa <prawee@hotmail.com>
2019-11-09 01:28:16 +0700

    update title style with margin buttom is 5

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -22,7 +22,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginLeft: 15
+    marginLeft: 15,
+    marginBottom: 5
   },
   container: {
     marginBottom: 10
