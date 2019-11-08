9bc09ea084bf1b79d42778881a4a5fb21d5788d3
Prawee Wongsa <prawee@hotmail.com>
2019-11-09 01:58:25 +0700

    send navigation props to child

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,7 @@ import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
 import ResultsList from '../components/ResultsList'
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
@@ -24,9 +24,18 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <ScrollView>
-        <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
-        <ResultsList results={filterResultsByPrice('$$')} title="Bit Priceier" />
-        <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+        <ResultsList 
+          navigation={navigation} 
+          results={filterResultsByPrice('$')} 
+          title="Cost Effective"/>
+        <ResultsList 
+          navigation={navigation} 
+          results={filterResultsByPrice('$$')} 
+          title="Bit Priceier" />
+        <ResultsList 
+          navigation={navigation} 
+          results={filterResultsByPrice('$$$')} 
+          title="Big Spender" />
       </ScrollView>
     </>
   )
