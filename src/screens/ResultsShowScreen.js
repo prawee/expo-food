f566e068c20cb0c7653f9edc9d1e3fae5ecb496a
Prawee Wongsa <prawee@hotmail.com>
2019-11-09 02:23:13 +0700

    using state for keep result from api

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import yelp from '../api/yelp'
 
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null)
   const id = navigation.getParam('id')
 
   const getResult = async (id) => {
     const response = await yelp.get(`/${id}`)
+    setResult(response.data)
   }
 
   return (
