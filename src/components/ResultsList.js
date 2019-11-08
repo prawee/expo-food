897f03098fa835a02d5b5fc6a1a42ae01097a64c
Prawee Wongsa <prawee@hotmail.com>
2019-11-09 01:03:52 +0700

    using ResultsDetail instead Text on renderItem

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, StyleSheet, FlatList } from 'react-native'
-
+import ResultsDetail from './ResultsDetail'
 
 const ResultsList = ({ title, results }) => {
   return (
@@ -12,7 +12,7 @@ const ResultsList = ({ title, results }) => {
         data={results}
         keyExtractor={(result) => result.id}
         renderItem={({ item }) => {
-          return <Text>{item.name}</Text>
+          return <ResultsDetail />
         }}
       />
     </View>
