f7aded3c94f54468b37fc2a01d46b831531fde15
Prawee Wongsa <prawee@hotmail.com>
2019-11-09 02:20:55 +0700

    create getResult helper for getting data from api

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import yelp from '../api/yelp'
 
 
 const ResultsShowScreen = ({ navigation }) => {
   const id = navigation.getParam('id')
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`)
+  }
+
   return (
     <View>
       <Text>Results Show</Text> 
